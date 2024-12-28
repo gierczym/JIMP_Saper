ec01f8691a8a8d5d12222f627c49d957b96b2baa
Michal Gierczynski <michal.gierczynski@pw.edu.pl>
2024-12-28 11:39:09 +0100

    Zmiana koloru na zielony

--- src/main.c
+++ src/main.c
@@ -5,9 +5,14 @@
 int main() {
 
 	enum command_t command; 
+
+	printf( "\033[0;32m"); // set color to green
 	while( ESC != (command = read_command()) ) {
 		printf("Wczytalem komende: ");
 		display_command( command );
 	}
+	
+	printf( "\033[0m"); // reset color to default
+
 	return 0;
 }
