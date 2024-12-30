da3763e2f5baefedba1f30ac69b998da444f32b8
Ariusz2137 <illuminati.kox@gmail.com>
2024-12-30 18:35:33 +0100

    dodano wybieranie poziomu truności

--- src/main.c
+++ src/main.c
@@ -8,12 +8,42 @@
 
 int main( int argc, char *argv[]) {
 
-	int board_size_x = 9;
-	int board_size_y = 9;
-	int n_mines = 10;
+	int board_size_x;
+	int board_size_y;
+	int n_mines;
 	int res;
 	enum command_t command; 
+	
+	printf("Witaj w grze saper!\npodaj poziom trudności\ne - łatwy\nm - średni\nh - trudny\nc - własny\n");
+	char difficulty = '\0';
+	while(difficulty == '\0'){
+		scanf("%c", &difficulty);
+		switch (difficulty){
+			case 'e':
+				board_size_x = 9;
+				board_size_y = 9;
+				n_mines = 10;
+				break;
+			case 'm':
+				board_size_x = 16;
+				board_size_y = 16;
+				n_mines = 40;
+				break;
+			case 'h':
+				board_size_x = 16;
+				board_size_y = 30;
+				n_mines = 99;
+				break;
+			case 'c':
+				//do zrobienia
+				return 0;
+			default:
+				printf("podano błędą wartość\npodaj poziom trudności\ne - łatwy\nm - średni\nh - trudny\nc - własny\n");
+				difficulty = '\0';
+				while (getchar() != '\n');
 
+		}
+	}
 	game_t game = initialize_game( board_size_x, board_size_y, n_mines );
 	if( argc > 1)
 		if( 0 == strcmp( argv[1], "--cheat") )
