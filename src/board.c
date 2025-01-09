2bb97b81d99d9c016613295a1a68268b4f89349d
Michal <michal.gierczynski@pw.edu.pl>
2025-01-09 14:35:12 +0100

    Dodalem plik colors.h

--- src/board.c
+++ src/board.c
@@ -3,18 +3,7 @@
 
 #include "board.h"
 
-#define SET_COLOR_GREEN	  printf( "\033[0;32m")
-#define SET_COLOR_BRIGHTGREEN	  printf( "\033[0;92m")
-#define SET_COLOR_RED	  printf( "\033[0;31m")
-#define SET_COLOR_BRIGHTRED	  printf( "\033[0;91m")
-#define SET_COLOR_BLUE	  printf( "\033[0;34m")
-#define SET_COLOR_BRIGHTBLUE	  printf( "\033[0;94m")
-#define SET_COLOR_MAGENTA	  printf( "\033[0;35m")
-#define SET_COLOR_CYAN	  printf( "\033[0;36m")
-#define SET_COLOR_YELLOW	  printf( "\033[0;33m")
-#define SET_COLOR_GREY	  printf( "\033[0;90m")
-#define SET_COLOR_DEFAULT printf( "\033[0m")
-#define SET_COLOR_BG_MAGENTA printf("\033[0;41m")
+#include "colors.h"
 
 /*
 typedef struct brd {
